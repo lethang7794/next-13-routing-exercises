7305779ee71b70dcc4d3c030d17521e67d46611d
Le Quang Thang <lethang7794@gmail.com>
2023-07-22 00:45:50 +0700

    Screensaver

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,28 @@
+import Link from 'next/link';
 import React from 'react';
 
-import ScreenSaver from '../../../components/ScreenSaver';
+const PATH = '/exercises/01-screensaver';
 
-function ScreenSaverExercise() {
+function ScreenSaverIndexPage() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <h1>Choose your color:</h1>
+      <ul>
+        <li>
+          <Link href={`${PATH}/hotpink`}>hotpink</Link>
+        </li>
+        <li>
+          <Link href={`${PATH}/lavender`}>lavender</Link>
+        </li>
+        <li>
+          <Link href={`${PATH}/white`}>white</Link>
+        </li>
+        <li>
+          <Link href={`${PATH}/red`}>red</Link>
+        </li>
+      </ul>
     </main>
   );
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverIndexPage;
