0118ebefe65fb0ed19d73312ca2635cf8519d021
Le Quang Thang <lethang7794@gmail.com>
2023-07-22 01:01:34 +0700

    Flash messages

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,15 +1,28 @@
 'use client';
-import React from 'react';
+import React, { useContext } from 'react';
+import { useRouter } from 'next/navigation';
+import { ToastContext } from '../../../../components/ToastProvider/ToastProvider';
 
 function ContactPage() {
+  const { createToast } = useContext(ToastContext);
+  const router = useRouter();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    router.push('/exercises/02-flash-messages');
+
+    createToast("Your message was received. We'l get back to you soon!", 'success');
+  }
+
   return (
     <main>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input id="name" required={true} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name:</label>
+        <input id='name' required={true} />
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" />
+        <label htmlFor='message'>Message:</label>
+        <textarea id='message' />
 
         <button>Submit</button>
       </form>
